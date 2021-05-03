5329923af648d1ec8f83be7264a39c362f4734be
Mark Thompson <the.mr.flibble@gmail.com>
2021-05-04 00:00:50 +0100

    Project setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 const mix = require('laravel-mix');
+const path = require('path');
+require('laravel-mix-bundle-analyzer');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,6 +13,38 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.webpackConfig({
+        devtool: 'source-map',
+        resolve: {
+            alias: {
+                'sass': path.resolve(__dirname, 'resources/sass'),
+            }
+        }
+    })
+    .sourceMaps()
+    .js('resources/js/app.js', 'public/js')
+    .vue({
+      extractStyles: 'public/css/vue.css',
+      // need mix 6 and webpack 5 :(
+      globalStyles: {
+        scss: [
+          'resources/sass/_variables.scss',
+          'node_modules/bootstrap/scss/_functions.scss',
+          'node_modules/bootstrap/scss/_variables.scss',
+          'node_modules/bootstrap/scss/_mixins.scss',
+        ],
+      }
+    })
+    .extract()
     .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+    .options({
+        processCssUrls: false,
+    })
+    .version();
+
+if (! mix.inProduction()) {
+    mix.bundleAnalyzer({
+        analyzerMode: 'static',
+        openAnalyzer: false,
+    });
+}
