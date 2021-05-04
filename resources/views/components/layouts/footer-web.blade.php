84c2fc2309fe29ed21cc678950fb221a64ca3e61
Mark Thompson <the.mr.flibble@gmail.com>
2021-05-04 19:38:25 +0100

    Re-order and complete the Footer

--- resources/views/components/layouts/footer-web.blade.php
+++ resources/views/components/layouts/footer-web.blade.php
@@ -1,26 +1,12 @@
 <footer class="web-footer">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-4 mb-2">
-                <a href="">TBC</a>
-
-                @if (config('app.env') != 'production')
-                <div>
-                    <span class="d-block d-sm-none">Breakpoint: xs</span>
-                    <span class="d-none d-sm-block d-md-none">Breakpoint: sm</span>
-                    <span class="d-none d-md-block d-lg-none">Breakpoint: md</span>
-                    <span class="d-none d-lg-block d-xl-none">Breakpoint: lg</span>
-                    <span class="d-none d-xl-block">Breakpoint: xl</span>
-                </div>
-                @endif
-            </div>
-
-            <div itemscope="" itemtype="http://schema.org/LocalBusiness" class="col-md-4 mb-2">
+            <div class="col-md-6 mb-2">
                 <p>All Rights Reserved.</p>
                 <p>&copy; themrflibble <?php echo date('Y');?></p>
                 <a href="#" class="toTop footer-title">Back To Top</a>
             </div>
-            <div class="col-md-4 follow mb-2">
+            <div class="col-md-6 follow mb-2">
                 <a href="https://www.linkedin.com/in/er-mark/" class="social-blk" target="blank">
                     <img src="/img/logo/icon-linkedin-opt.png" class="social-blk" alt="Linked In logo">
                 </a>
@@ -33,6 +19,17 @@
                 <a href="https://www.buymeacoffee.com/bomo" class="social-blk" target="blank">
                     <img src="/img/logo/icon-coffee-opt.png" class="social-blk" alt="Buy Me A Coffee logo">
                 </a>
+
+                @if (config('app.env') != 'production')
+                <div class="mt-2">
+                    <span class="d-block d-sm-none">Breakpoint: xs</span>
+                    <span class="d-none d-sm-block d-md-none">Breakpoint: sm</span>
+                    <span class="d-none d-md-block d-lg-none">Breakpoint: md</span>
+                    <span class="d-none d-lg-block d-xl-none">Breakpoint: lg</span>
+                    <span class="d-none d-xl-block">Breakpoint: xl</span>
+                </div>
+                @endif
+
             </div>
         </div>
     </div>
